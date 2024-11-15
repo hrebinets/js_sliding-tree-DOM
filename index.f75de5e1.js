88acec8f0e9df48a9877dc9bc6a4896f5ab654b4
Max Hrebinets <brainisbro@Gmail.com>
2024-11-15 09:54:56 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const list = document.querySelector("ul");
+list.querySelectorAll("li").forEach((el)=>{
+    const span = document.createElement("span");
+    span.textContent = el.firstChild.textContent.trim();
+    el.firstChild.replaceWith(span);
+});
+list.addEventListener("click", (e)=>{
+    if (e.target.tagName === "SPAN") {
+        const listItem = e.target.closest("li");
+        const subList = listItem.querySelector("ul");
+        if (subList) subList.style.display = subList.style.display === "none" ? "" : "none";
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
